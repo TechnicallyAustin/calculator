c572212651de4c39b7be232e253afa0ad0535db5
TechnicallyAustin <TechnicallyAustin@gmail.com>
2023-01-18 16:19:09 -0700

    working on math

--- src/calc.js
+++ src/calc.js
@@ -1,6 +1,7 @@
 function calculator() {
-  let firstInput;
-  let secondInput;
+  let firstInput = "";
+  let secondInput = "";
+  let runningTotal = ""; // stores the current display as a string of numbers
 
   let result = document.querySelector(".result-value");
   let previousResult = document.querySelector(".previous-result");
@@ -14,9 +15,15 @@ function calculator() {
     for (let i = 0; i < numbers.length; i++) {
       let number = numbers[i];
       let value = number.textContent;
-      number.addEventListener("click", () => {
-        result.textContent += value;
-      });
+      number.addEventListener("click", (event) => {
+        // creates a string of the numbers clicked
+            runningTotal += value
+        // updates the result display with the running total
+            result.textContent = runningTotal
+
+            console.log("running total", runningTotal)
+      })
+
     }
   }
   numberButtons();
@@ -31,13 +38,14 @@ function calculator() {
         switch (value) {
           case "AC":
             previousResult.textContent = " ";
-            secondInput = 0;
+            secondInput = ""
+            runningTotal = " "
             console.log(secondInput);
             break;
 
           case "C":
             result.textContent = " ";
-            firstInput = 0;
+            firstInput = ""
             console.log(firstInput);
             break;
 
@@ -60,6 +68,23 @@ function calculator() {
   function math(operator){
     switch (operator){
         case "+":
+            // sets the first input to a number
+            let a = parseInt(firstInput)
+            // sets the second input tp a number
+            let b = parseInt(secondInput)
+            // sets the sum to the sum of a + b
+            let sum = a + b
+
+            // updates the result with the sum
+            result.textContent = sum
+            
+            //sets running total to sum
+            return sum
+
+            console.log("RT", runningTotal, "FI", firstInput, "SI", secondInput)
+
+
+            console.log("math function add", "sum", sum)
             break;
 
         case "-":
@@ -78,13 +103,35 @@ function calculator() {
 
   function operatorButtons() {
     // sets first input when operator is clicked.
-    console.log("operator out of loop:", "FI", firstInput, "SI", secondInput);
+
 
     for (let i = 0; i < operators.length - 1; i++) {
       let operator = operators[i];
       let value = operator.textContent;
 
       operator.addEventListener("click", () => {
+        if (result.textContent !== " "){
+            firstInput = runningTotal
+            console.log("FI", firstInput)
+        }
+        //set first input to running total
+
+
+        // sets running total to an empty string
+        runningTotal = " "
+
+        // sets the previous operator
+        previousOperator = value
+
+        // set the previous display to the firstInput + previousOperator
+        previousResult.textContent = `${firstInput + " "} ${previousOperator}`
+
+        // clear the result
+        result.textContent = " "
+
+
+
+            //console.log("operator value:",value, "FI", firstInput, "SI", secondInput);
         evaluate(value)
       })
     }
@@ -94,13 +141,26 @@ function calculator() {
   function evaluate(previousOperator) {
     const equalButton = document.querySelector(".equals");
     const value = equalButton.textContent;
+    let mathResult
 
         equalButton.addEventListener("click", () => {
-            secondInput = parseInt(result.textContent);
-            console.log("= clicked", "first input", firstInput, "previousOperator", previousOperator, "second Input", secondInput)
+            secondInput = runningTotal
           switch (previousOperator) {
             case "+":
-                console.log(value)
+                runningTotal = math(previousOperator)
+
+
+                 previousResult.textContent = `${firstInput + " "} ${
+                   previousOperator + " "
+                 } ${secondInput + " "} ${value + " "} ${runningTotal} `;
+
+                 
+
+
+
+                
+
+
             break;
 
             case "-":
@@ -120,7 +180,9 @@ function calculator() {
           }; 
 
 
-          console.log("operator = loop:","FI", firstInput, "SI", secondInput, "PR", previousOperator);
+
+
+          console.log("operator = loop:","FI", firstInput, "SI", secondInput, "PR", previousOperator, "result", mathResult );
           // sets second input when operator is clicked.
         });
     
